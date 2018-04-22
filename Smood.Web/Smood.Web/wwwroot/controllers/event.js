c50a495aa39fc3f1106a8713b047bbf586a87a37
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-04-22 04:34:40 +0100

    upload service on apiConnector

--- Smood.Web/Smood.Web/wwwroot/controllers/event.js
+++ Smood.Web/Smood.Web/wwwroot/controllers/event.js
@@ -69,25 +69,15 @@
                 $rootScope.hideLoadingAnimation(true);
             });
 
-            $scope.uploadImages = ($files) => {
-
-                var filesPromise = Upload.base64DataUrl($files);
-                var postPromise = filesPromise.then( filesData => {
-                    filesData = filesData.map( fd => { 
-                        fd = fd.replace(/^data:image\/(png|jpg|jpeg|gif);base64,/, "")
-                        return fd;
-                    });                    
-                    return apiConnector.postImage($routeParams.id, "event", filesData)
-                });
-
-                postPromise.then( result => {
+            $scope.uploadImages = ($files) => {                
+                apiConnector.postImage($routeParams.id, "event", $files)
+                .then( result => {
                     console.log(result);
                 })
                 .catch(err => {
                     $location.path("/error");
                     $rootScope.hideLoadingAnimation(true);                    
                 });
-
             };
 
             var _submit = submitEvent => {
