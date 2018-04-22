aaa582ad1afc70566de45ac5d9bb4acdf6232e7e
Miguel Ângelo Simões Freitas <mfreitas@student.dei.uc.pt>
2018-04-22 07:11:08 +0100

    +Add datetimepicker input

--- Smood.Web/Smood.Web/wwwroot/directives/input-custom.js
+++ Smood.Web/Smood.Web/wwwroot/directives/input-custom.js
@@ -8,15 +8,39 @@
                 model: '=',
                 id: '@',
                 label: '@',
-                type: '@'                
+                type: '@',
+                placeholder: '@',
+                inputId: '@'
             },
-            controller: function($scope) {
+            controller: function($scope, $timeout) {
                 if(!$scope.type) {
                     $scope.type = "text";
                 }
+
+                if (!$scope.placeholder) {
+                    $scope.placeholder = $scope.label;
+                }
+
+                if ($scope.type == "dateAndPlaceholder") {
+                    $timeout(() => {
+                        console.log($scope.fatherId + " #" + $scope.inputId);
+                        $($scope.fatherId+" #" + $scope.inputId).datetimepicker({
+                            locale: 'pt'
+                        });
+                    });
+                }
+
+                $scope.onUnSelect = e => {
+
+                };
+
             },
-            templateUrl: function() {
-                return 'views-directives/input-custom.html';
+            templateUrl: function (elem, attrs) {
+                if (attrs.type == 'dateAndPlaceholder') {
+                    return 'views-directives/date-input-placeholder.html';
+                } else {
+                    return 'views-directives/input-custom.html';
+                }
             }            
         }
     })
