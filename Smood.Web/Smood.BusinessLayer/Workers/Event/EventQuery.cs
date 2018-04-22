cbe78e4fab768c237cf75eab36e2ad5f18040aa6
Pedro Emanuel Sousa <pesousa@ipn.pt>
2018-04-22 03:56:07 +0100

    Post photo partial fix; Elastic search link by configuration;

--- Smood.Web/Smood.BusinessLayer/Workers/Event/EventQuery.cs
+++ Smood.Web/Smood.BusinessLayer/Workers/Event/EventQuery.cs
@@ -11,7 +11,29 @@ namespace Smood.BusinessLayer.Workers.Event
 {
     public class EventQuery : BaseWorker
     {
-        public EventQuery(DatabaseContext context) : base(context) {}
+        #region Constructor
+
+        public EventQuery(DatabaseContext context) : base(context) {
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private StringResponse DoElasticSearch(string _elasticUrl, object postData)
+        {
+            var settings = new ConnectionConfiguration(new Uri(_elasticUrl))
+                                .RequestTimeout(TimeSpan.FromMinutes(2));
+
+            var lowlevelClient = new ElasticLowLevelClient(settings);
+
+            return lowlevelClient.Search<StringResponse>("shift-simplified", PostData.Serializable(postData));
+        }
+
+        #endregion
+
+        #region CRUD
 
         public IQueryable<EventListDTO> GetAll(string basePath)
         {
@@ -43,17 +65,21 @@ namespace Smood.BusinessLayer.Workers.Event
             }).FirstOrDefault();
         }
 
-        public ChartDTO GetEmotionsByRange(int eventId, DateTime startDate, DateTime endDate)
+        #endregion
+
+        #region Elastic Search
+
+        public ChartDTO GetEmotionsByRange(int eventId, DateTime startDate, DateTime endDate, string _elasticUrl)
         {
             // TODO: filter elasticsearch by event code
             var @event = DatabaseContext.Events.FirstOrDefault(e => e.EventId == eventId && e.DeleteDate == null);
 
-            var settings = new ConnectionConfiguration(new Uri("http://206.189.30.185:9200"))
+            var settings = new ConnectionConfiguration(new Uri(_elasticUrl))
                                 .RequestTimeout(TimeSpan.FromMinutes(2));
 
             var lowlevelClient = new ElasticLowLevelClient(settings);
 
-            var searchResult = lowlevelClient.Search<StringResponse>("shift-simplified", PostData.Serializable(new
+            var searchResult = DoElasticSearch(_elasticUrl, new
             {
                 size = 0,
                 query = new
@@ -144,7 +170,7 @@ namespace Smood.BusinessLayer.Workers.Event
                         }
                     }
                 }
-            }));
+            });
 
             //if ()
             //{
@@ -238,5 +264,107 @@ namespace Smood.BusinessLayer.Workers.Event
 
             return finalResponse;
         }
+
+        public ChartDTO GetAverageAge(int eventId, string _elasticUrl)
+        {
+            var searchResult = DoElasticSearch(_elasticUrl, new
+            {
+                size = 0,
+            });
+
+            //if ()
+            //{
+            var jsonResponse = JObject.Parse(searchResult.Body);
+            var buckets = jsonResponse["aggregations"]["groupByHour"]["buckets"].ToArray();
+
+            var finalResponse = new ChartDTO
+            {
+                Labels = new List<string>(),
+                Series = new List<ChartSeriesDTO>()
+            };
+
+
+            var happyList = new List<decimal?>();
+            var angryList = new List<decimal?>();
+            var sadList = new List<decimal?>();
+            var unknownList = new List<decimal?>();
+            var calmList = new List<decimal?>();
+            var digustedList = new List<decimal?>();
+            var surprisedList = new List<decimal?>();
+            var confusedList = new List<decimal?>();
+
+            foreach (var bucket in buckets)
+            {
+                var label = bucket["key_as_string"].ToString();
+                finalResponse.Labels.Add(label);
+
+                var happyValue = bucket["avgHappy"]["value"].ToString();
+                var angryValue = bucket["avgAngry"]["value"].ToString();
+                var sadValue = bucket["avgSad"]["value"].ToString();
+                var unknownValue = bucket["avgUnknown"]["value"].ToString();
+                var calmValue = bucket["avgCalm"]["value"].ToString();
+                var digustedValue = bucket["avgDigusted"]["value"].ToString();
+                var surprisedValue = bucket["avgSurprised"]["value"].ToString();
+                var confusedValue = bucket["avgConfused"]["value"].ToString();
+
+
+                happyList.Add(happyValue == "" ? (decimal?)null : Convert.ToDecimal(happyValue));
+                angryList.Add(angryValue == "" ? (decimal?)null : Convert.ToDecimal(angryValue));
+                sadList.Add(sadValue == "" ? (decimal?)null : Convert.ToDecimal(sadValue));
+                unknownList.Add(unknownValue == "" ? (decimal?)null : Convert.ToDecimal(unknownValue));
+                calmList.Add(calmValue == "" ? (decimal?)null : Convert.ToDecimal(calmValue));
+                digustedList.Add(digustedValue == "" ? (decimal?)null : Convert.ToDecimal(digustedValue));
+                surprisedList.Add(surprisedValue == "" ? (decimal?)null : Convert.ToDecimal(surprisedValue));
+                confusedList.Add(confusedValue == "" ? (decimal?)null : Convert.ToDecimal(confusedValue));
+            }
+
+
+            finalResponse.Series = new List<ChartSeriesDTO>
+            {
+                new ChartSeriesDTO {
+                    Label = "Happy",
+                    Data = happyList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Angry",
+                    Data = angryList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Sad",
+                    Data = sadList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Unknown",
+                    Data = unknownList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Calm",
+                    Data = calmList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Digusted",
+                    Data = digustedList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Surprised",
+                    Data = surprisedList
+                },
+                new ChartSeriesDTO
+                {
+                    Label = "Confused",
+                    Data = confusedList
+                }
+            };
+
+            return finalResponse;
+        }
+
+        #endregion
     }
 }
--- Smood.Web/Smood.Web/Base/Settings/SmoodSettings.cs
+++ Smood.Web/Smood.Web/Base/Settings/SmoodSettings.cs
@@ -3,5 +3,7 @@
     public class SmoodSettings
     {
         public string ImageUploadFolder { get; set; }
+
+        public string ElasticSearchLink { get; set; }
     }
 }
